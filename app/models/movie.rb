ac111795e13f2a2cb239c86929eef07bb1379c49
KellyMark <kmark602@gmail.com>
2023-05-25 20:46:37 -0400

    commit tree 28b0c91692c0aa55b1f6ba7c48fe8bde03861011

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,55 @@
 class Movie < ActiveRecord::Base
 
+    #Create
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
+
+    #Read
+    def self.first_movie
+        Movie.all.first
+    end
+
+    def self.last_movie
+        Movie.all.last
+    end
+
+    def self.movie_count
+        Movie.count
+    end
+    
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes(attributes)
+        Movie.find_by(attributes)
+    end
+
+    def self.find_movies_after_2002
+        Movie.where('release_date > 2002')
+    end
+
+    #Update
+    def update_with_attributes(updated_attribute)
+        self.update(updated_attribute)
+    end
+
+    def self.update_all_titles(new_title)
+        Movie.all.update(title: new_title)
+    end
+
+    #Delete
+
+    def self.delete_by_id(id)
+        Movie.find(id).delete
+    end
+
+    def self.delete_all_movies
+        Movie.delete_all
+    end
+
 end
+
+# binding.pry
+# 0
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 ENV["RACK_ENV"] ||= "development"
-
+require 'pry'
 require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
